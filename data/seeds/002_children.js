d2b1eea285c8798d760264f01b446ca67acc1cdd
Maria Negri <mariamnegri@gmail.com>
2022-04-28 17:29:26 -0300

    Added editChildren migration to change parentId format to string

--- data/seeds/002_children.js
+++ data/seeds/002_children.js
@@ -3,9 +3,24 @@ exports.seed = function (knex) {
     .truncate()
     .then(function () {
       return knex('children').insert([
-        { id: 1, parentId: 63, name: 'Claire Marie', pin: 1234 },
-        { id: 2, parentId: 33, name: 'Audrey Belle', pin: 1234 },
-        { id: 3, parentId: 86, name: 'Marcus Aurelius', pin: 1234 },
+        {
+          id: '1',
+          parentId: '614025c94fec6d00682bf2a6',
+          name: 'Claire Marie',
+          pin: 1234,
+        },
+        {
+          id: '2',
+          parentId: '614025c94mmm6d00682bf2a6',
+          name: 'Audrey Belle',
+          pin: 1234,
+        },
+        {
+          id: '3',
+          parentId: '614025c94nnn6d00682bf2a6',
+          name: 'Marcus Aurelius',
+          pin: 1234,
+        },
       ]);
     });
 };
