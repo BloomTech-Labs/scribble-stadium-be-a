e9e41b9efeab68722832c0c4fb8d67dcb29ceb4d
markporo <markporo@yahoo.com>
2022-05-11 03:17:09 -0400

    deleted the ids from submissions and submissionPages so the ids can auto increment

--- data/seeds/004_submissionPages.js
+++ data/seeds/004_submissionPages.js
@@ -2,21 +2,18 @@ const { faker } = require('@faker-js/faker');
 
 const submissionPages = [
   {
-    id: 1,
     submissionId: faker.datatype.number(),
     type: faker.random.word(),
     url: faker.internet.url(),
     pageNum: faker.datatype.number(),
   },
   {
-    id: 2,
     submissionId: faker.datatype.number(),
     type: faker.random.word(),
     url: faker.internet.url(),
     pageNum: faker.datatype.number(),
   },
   {
-    id: 3,
     submissionId: faker.datatype.number(),
     type: faker.random.word(),
     url: faker.internet.url(),
