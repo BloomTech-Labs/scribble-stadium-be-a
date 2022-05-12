3228717783aec9ac8d4b837e35493c6eaaf5ca7b
markporo <markporo@yahoo.com>
2022-05-12 03:14:13 -0400

    I changed the children migration to have an incrementing id.  Also went ahead and deleted the id fields in the seeds...all to allow the automatic creation of child ids

--- data/seeds/002_children.js
+++ data/seeds/002_children.js
@@ -4,19 +4,16 @@ exports.seed = function (knex) {
     .then(function () {
       return knex('children').insert([
         {
-          id: '1',
           parentId: '614025c94fec6d00682bf2a6',
           name: 'Claire Marie',
           pin: 1234,
         },
         {
-          id: '2',
           parentId: '614025c94mmm6d00682bf2a6',
           name: 'Audrey Belle',
           pin: 1234,
         },
         {
-          id: '3',
           parentId: '614025c94nnn6d00682bf2a6',
           name: 'Marcus Aurelius',
           pin: 1234,
