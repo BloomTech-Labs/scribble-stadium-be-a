9321770829a0cba58e110a8a108c7a63cbbb4d52
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-05-17 18:10:18 -0700

    fixed console.log

--- api/stories/storiesMiddleware.js
+++ api/stories/storiesMiddleware.js
@@ -1,9 +1,7 @@
 const Stories = require('./storiesModel.js');
 
 async function checkId(req, res, next) {
-  console.log(
-    '______________________________________....@@@@@@@@@@@ CHECK ID MIDDLEWARE @@@@@@@@@@@.......'
-  );
+  // console.log('@@@@@ CHECK ID MIDDLEWARE @@@@');
   try {
     const id = await Stories.getStoryById(req.params.id);
     if (!id) res.status(404).json({ message: `ID: ${id} not found` });
@@ -13,18 +11,6 @@ async function checkId(req, res, next) {
   }
 }
 
-// async function checkStoryId(req, res, next) {
-//   const id = req.params.storyId;
-//   try {
-//     const episodes = await Stories.getEpisodesByStoryID(id);
-//     if (!episodes) res.status(404).json({ message: 'story id not found' });
-//     else next();
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// }
-
 module.exports = {
   checkId,
-  // checkStoryId
 };
