386a8e77a7e75c73d94723587c8391f40d1485aa
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-05-21 11:16:41 -0700

    finished prompts testing suit

--- __tests__/routes/storyEpisodePrompts.test.js
+++ __tests__/routes/storyEpisodePrompts.test.js
@@ -16,11 +16,12 @@ jest.mock('../../api/middleware/authProfile', () => {
 // Parent test suite*
 describe('storyEpisodePrompts router endpoints', () => {
   beforeAll(() => {
-    server.use('/storyEpisodePrompts', storyEpisodePromptsRouter); // root route for the router
+    server.use('/storyEpisodePrompts', storyEpisodePromptsRouter);
     jest.clearAllMocks();
   });
 
   describe('POST /storyEpisodePrompts', () => {
+
     it('should return 201 when prompt is created', async () => {
       const mockPrompts = {
         episodeId: 89,
@@ -29,12 +30,10 @@ describe('storyEpisodePrompts router endpoints', () => {
         created_at: '2022-05-18T01:42:25.658Z',
         updated_at: '2022-05-18T01:42:25.658Z',
       };
-
       Prompts.addEpisodePrompt.mockResolvedValue({
         ...mockPrompts,
         id: 64,
       });
-
       const res = await request(server)
         .post('/storyEpisodePrompts')
         .send([mockPrompts]);
@@ -42,11 +41,10 @@ describe('storyEpisodePrompts router endpoints', () => {
     });
   });
 
-
-
   describe('GET /storyEpisodePrompts/episodeId', () => {
+
     it('should return 200 when prompts found', async () => {
-      Prompts.getPromptsByEpisodeId.mockResolvedValue([
+      Prompts.getEpisodePromptByEpisodeId.mockResolvedValue([
         {
           id: 64,
           episodeId: 89,
@@ -57,22 +55,13 @@ describe('storyEpisodePrompts router endpoints', () => {
         },
       ]);
       const res = await request(server).get('/storyEpisodePrompts/89');
-      console.log('@@@@@@@@@@@@@ res.body =', res.body);
-
       expect(res.status).toBe(200);
     });
+
+    it('should return 404 when no prompts are found', async () => {
+      Prompts.getEpisodePromptByEpisodeId.mockResolvedValue();
+      const res = await request(server).get('/storyEpisodePrompts/89');
+      expect(res.status).toBe(404);
+    });
   });
-
 }); // end of parent test suite
-
-// describe('GET /stories', () => {
-//   it('should return 200', async () => {
-//     Stories.getAllStories.mockResolvedValue([]); // [] becomes res.body // We can create anything here! Does not need to be [].
-//     const res = await await request(server).get('/stories');
-//     // console.log('@@@@@@@@@@', res.body); // []
-//     const storiesLength = Stories.getAllStories.mock.calls.length; // mock.calls =[[]] mock.calls.length = 1
-//     expect(res.status).toBe(200);
-//     expect(res.body.length).toBe(0); // Empty array unless we state otherwise.
-//     expect(storiesLength).toBe(1);
-//   });
-// }); // End of GET /stories
