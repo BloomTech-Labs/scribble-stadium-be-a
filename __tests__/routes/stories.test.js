5bb1c0e15c6e4b600b1f6b16a449f27bdcbfee84
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-05-21 11:23:27 -0700

    finished stories tests

--- __tests__/routes/stories.test.js
+++ __tests__/routes/stories.test.js
@@ -5,19 +5,7 @@ const storiesRouter = require('../../api/stories/storiesRouter');
 const server = express();
 server.use(express.json());
 
-// stories = {
-//   id: '7',
-//   title: 'text',
-//   description: 'text',
-//   author: 'text',
-// }
-
-// 47 min of testing video
-// https://www.youtube.com/watch?v=hpIf6GG7B9c&list=PL0pqmT38sWfthShZSffvgJp7MapFC0Ao_&index=65&t=1462s
-// answers video = https://bloomtech-1.wistia.com/medias/5ylj9llq4c
-
-jest.mock('../../api/stories/storiesModel'); // Should we mock routes too?
-// mock the auth middleware completely
+jest.mock('../../api/stories/storiesModel');
 jest.mock('../../api/middleware/authProfile', () => {
   return {
     auth0Verify: jest.fn((req, res, next) => next()),
@@ -25,42 +13,34 @@ jest.mock('../../api/middleware/authProfile', () => {
   };
 });
 
-// Parent test suite*
+// Parent test suite:
 describe('stories router endpoints', () => {
   beforeAll(() => {
-    // This is the module/route being tested
     server.use('/stories', storiesRouter);
     jest.clearAllMocks();
   });
 
   describe('GET /stories', () => {
     it('should return 200', async () => {
-      Stories.getAllStories.mockResolvedValue([]); // [] becomes res.body // We can create anything here! Does not need to be [].
+      Stories.getAllStories.mockResolvedValue([]);
       const res = await await request(server).get('/stories');
-      // console.log('@@@@@@@@@@', res.body); // []
-      const storiesLength = Stories.getAllStories.mock.calls.length; // mock.calls =[[]] mock.calls.length = 1
+      const storiesLength = Stories.getAllStories.mock.calls.length;
       expect(res.status).toBe(200);
-      expect(res.body.length).toBe(0); // Empty array unless we state otherwise.
+      expect(res.body.length).toBe(0);
       expect(storiesLength).toBe(1);
     });
-  }); // End of GET /stories
+  });
 
   describe('GET /stories/:id', () => {
     it('should return 200 when story found', async () => {
       Stories.getStoryById.mockResolvedValue([
-        // Remember the array*
         {
           title: 'text',
           description: 'word',
         },
       ]);
       const res = await request(server).get('/stories/99');
-      // console.log('@@@@@@----->', res.body);
-      // Per Jake: You aren't testing the knex query or the DB connection,
-      // just that if you get something back the route returns 200.
-      // You could add that it returns a body as well
       expect(res.status).toBe(200);
-      // expect(Stories.getStoryById.mock.calls.length).toBe(2);
     });
 
     it('should return 404 when story not found', async () => {
@@ -70,19 +50,17 @@ describe('stories router endpoints', () => {
     });
 
     it("should have title and a description keys in it's object.", async () => {
-      Stories.getStoryById.mockResolvedValue([
-        // Remember the array*
+      Stories.getStoryById.mockResolvedValue(
         {
           title: 'text',
           description: 'word',
         },
-      ]);
+      );
       const res = await request(server).get('/stories/99');
-      // console.log('$$$$$$$$----->', res.body);
-      expect(res.body.title).toBe('text'); // Passes if body.title is 'text', fails if not.
+      expect(res.body.title).toBe('text');
       expect(res.body.description).toBe('word');
     });
-  }); // End of GET /stories/:id
+  });
 
   describe('PUT /stories/:id', () => {
     it('should return 204 when updated', async () => {
@@ -95,11 +73,7 @@ describe('stories router endpoints', () => {
       Stories.getStoryById.mockResolvedValue([story]);
       const res = await request(server).put('/stories/1').send(story);
       expect(res.status).toBe(204);
-      // console.log('@@@@@@@@@@@@@@', res.status);
     });
-
-    // it("should return 404 when not found", async () => {
-    // });
   });
 
 }); // End of parent test suite
