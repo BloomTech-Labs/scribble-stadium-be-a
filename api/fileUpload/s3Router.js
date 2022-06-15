2224678ea286a8f233f551c406a0463294d61a47
Joe Hernandez <j.t.hernandez999@gmail.com>
2022-06-14 20:07:41 -0700

    set up aws config

--- api/fileUpload/s3Router.js
+++ api/fileUpload/s3Router.js
@@ -1,4 +1,13 @@
+const aws = require('aws-sdk');
+require('dotenv').config();
+
 const router = require('express').Router();
+
+aws.config.update({
+  region: 'us-east-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+});
 
 router.get('/', (req, res) => {
   console.log('sign_s3');
