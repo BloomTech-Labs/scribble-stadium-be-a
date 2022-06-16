8e1558cdb142cef8fd171713e2466f5e12bed7d6
Chase Donovan <chase.donovan@aol.com>
2022-06-15 20:10:50 -0700

    added logic in s3Router to retrieve signed url from test bucket and edited app.js to work with updated route

--- api/fileUpload/s3Router.js
+++ api/fileUpload/s3Router.js
@@ -1,7 +1,7 @@
 const aws = require('aws-sdk');
 require('dotenv').config();
 
-const router = require('express').Router();
+// const router = require('express').Router();
 
 aws.config.update({
   region: 'us-east-1',
@@ -9,8 +9,38 @@ aws.config.update({
   secretAccessKey: process.env.AWS_SECRET_KEY,
 });
 
-router.get('/', (req, res) => {
-  console.log('sign_s3');
-  res.json('success from s3 Router');
-});
-module.exports = router;
+const S3_BUCKET = process.env.BUCKET;
+exports.sign_s3 = (req, res) => {
+  const s3 = new aws.S3(); // Create a new instance of S3
+  const fileName = 'bikeImg.jpg'; // Set the file name to bikeImg.jpg to reference the img in a test bucket
+  const fileType = req.body.fileType;
+  // Set up the payload of what we are sending to the S3 api
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 500,
+    ContentType: fileType,
+    ACL: 'public-read',
+  };
+  // Make a request to the S3 API to get a signed URL which we can use to upload our file
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.json({ success: false, error: err });
+    }
+    // Data payload of what we are sending back, the url of the signedRequest and a URL where we can access the content after its saved.
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+    };
+    // Send it all back
+    res.json({ success: true, data: { returnData } });
+  });
+};
+
+// router.get('/', (req, res) => {
+//   console.log('sign_s3');
+//   res.json('success from s3 Router');
+// });
+
+// module.exports = router;
