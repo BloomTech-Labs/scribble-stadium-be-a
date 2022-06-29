5767fd409f8a23e25e104fd89ddc770d0bb8fb37
ashtilawat23 <ashtilawat23@gmail.com>
2022-06-28 21:47:49 -0400

    fixed profile and children seeds

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,39 +1,46 @@
-const { faker } = require('@faker-js/faker');
-
-// const fakeProfiles = [...new Array(4)].map((i, idx) => ({
-//   id: idx === 0 ? '614025c94fec6d00682bf2a6' : faker.random.alphaNumeric(20),
-//   avatarUrl: faker.image.avatar(),
-//   email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
-//   name:
-//     idx === 0
-//       ? 'Test001 User'
-//       : `${faker.name.firstName()} ${faker.name.lastName()}`,
-// }));
-
-const parentProfile = {
-  id: 'auth0|618aee9bc49d5800691da3ee',
-  avatarUrl: faker.image.avatar(),
-  email: 'llama001@maildrop.cc',
-  name: 'Marty McFly',
-  role: 'parent',
-};
-
-const adminProfile = {
-  id: 'auth0|628288c99e688d006929c236',
-  avatarUrl: faker.image.avatar(),
-  email: 'llama002@maildrop.cc',
-  name: 'Darwin Graig',
-  role: 'admin',
-};
-
-const profiles = [parentProfile, adminProfile];
+/*
+  Manually setting the `id` for each profile to the Auth0 provided ID. Adding
+  profiles was not in scope for this iteration, but adding profiles in the 
+  future will require the okta-id to be set as the `id` for each profile.
+*/
+profiles = [
+  {
+    email: 'llama001@maildrop.cc',
+    name: 'Test001 User',
+    id: 'auth0|614025c94fec6d00682bf2a6',
+    role: 'parent',
+    avatarUrl: 'https://s.gravatar.com/avatar/dca14075e1a5060bc748cddf77cd2c9c?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fll.png',
+  },
+  {
+    email: 'llama002@maildrop.cc',
+    name: 'Test002 User',
+    id: 'auth0|62bb71f456952cd3edab1f35',
+    role: 'parent',
+    avatarUrl: 'https://s.gravatar.com/avatar/cbea34f8cb8b8766baa9f18b4cdd9e73?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fll.png',
+  },
+  {
+    email: 'llama003@maildrop.cc',
+    name: 'Test003 User',
+    id: 'auth0|62bb721a8ece54f5737a398c',
+    role: 'parent',
+    avatarUrl: 'https://s.gravatar.com/avatar/db4516fb09d1066c4bd303fe0bd82fa9?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fll.png',
+  },
+  {
+    email: 'llama004@maildrop.cc',
+    name: 'Test004 User',
+    id: 'auth0|62bb723a112fd45937b86821',
+    role: 'parent',
+    avatarUrl: 'https://s.gravatar.com/avatar/0377b2b316037afb070f68beaae17bf7?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fll.png',
+  },
+  {
+    email: 'llama007@maildrop.cc',
+    name: 'Test007 User',
+    id: 'auth0|614a6b0c8f29ac00711b6abd',
+    role: 'admin',
+    avatarUrl: 'https://s.gravatar.com/avatar/94d571a39cddffd878490238a04155da?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fll.png',
+  },
+];
 
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('profiles')
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('profiles').insert(profiles);
-    });
+  return knex('profiles').insert(profiles);
 };
--- data/seeds/002_children.js
+++ data/seeds/002_children.js
@@ -4,18 +4,48 @@ exports.seed = function (knex) {
     .then(function () {
       return knex('children').insert([
         {
-          parentId: 'auth0|618aee9bc49d5800691da3ee',
+          parentId: 'auth0|614025c94fec6d00682bf2a6',
           name: 'Claire Marie',
           pin: 1234,
         },
         {
-          parentId: 'auth0|618aee9bc49d5800691da3ee',
+          parentId: 'auth0|614025c94fec6d00682bf2a6',
           name: 'Audrey Belle',
           pin: 1234,
         },
         {
-          parentId: 'auth0|618aee9bc49d5800691da3ee',
+          parentId: 'auth0|614025c94fec6d00682bf2a6',
           name: 'Marcus Aurelius',
+          pin: 1234,
+        },
+        {
+          parentId: 'auth0|62bb71f456952cd3edab1f35',
+          name: 'Blair Sinclair',
+          pin: 1234,
+        },
+        {
+          parentId: 'auth0|62bb71f456952cd3edab1f35',
+          name: 'Dustin Sinclair',
+          pin: 1234,
+        },
+        {
+          parentId: 'auth0|62bb721a8ece54f5737a398c',
+          name: 'Muffy Crowntop',
+          pin: 1234,
+        },
+        {
+          parentId: 'auth0|62bb721a8ece54f5737a398c',
+          name: 'Ben Crowntop',
+          pin: 1234,
+        },
+        {
+          parentId: 'auth0|62bb723a112fd45937b86821',
+          name: 'John Smith',
+          pin: 1234,
+        },
+        {
+          parentId: 'auth0|62bb723a112fd45937b86821',
+          name: 'Ruby Smith',
           pin: 1234,
         },
       ]);
