961e21c67511fa8e12c1e9818da1e3068b6bb7a2
Joe Hernandez <j.t.hernandez999@gmail.com>
2022-07-25 20:21:41 -0700

    completed swagger on submissions page route

--- api/childSubmissions/childSubmissionsRouter.js
+++ api/childSubmissions/childSubmissionsRouter.js
@@ -42,12 +42,12 @@ const { aws, s3UploadBucket } = require('../../config/aws');
  *                 childId: 1
  *                 storyId: 1
  *                 episodeId: 1
- *                 "approvedAt": null,
+ *                 approvedAt: null
  *                 episodeStartDate: "2021-12-12T20:20:16.719Z"
  *                 finishedReadingAt: null
  *                 finishedWritingAt: null
  *                 squadCreatedAt: "2022-01-05T12:29:22.037Z"
- *                 votedAt: null,
+ *                 votedAt: null
  *                 createdAt: 2021-10-08 19:13:54.822+00
  *                 updatedAt: 2021-10-08 19:13:54.822+00
  */
@@ -232,7 +232,6 @@ router.post(
       ContentType: fileType,
       ACL: 'public-read',
     };
-
     // Make a request to the S3 API to get a signed URL which we can use to upload our file
     s3.getSignedUrl('putObject', s3Params, (err, data) => {
       if (err) {
@@ -249,8 +248,66 @@ router.post(
     });
   }
 );
+/**
+ * @swagger
+ * /page:
+ *  post:
+ *    summary: Save submission page
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - submissions
+ *    requestBody:
+ *      description: Submission page object with submissionId, type, url, and pageNum
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - submissionId
+ *              - type
+ *              - url
+ *              - pageNum
+ *            properties:
+ *              submissionId:
+ *                type: integer
+ *              type:
+ *                type: string
+ *              url:
+ *                type: string
+ *              pageNum:
+ *                type: integer
+ *            example:
+ *                submissionId: 2
+ *                type: "drawing"
+ *                url: "https://bucketeer-0fcbcf8b-2b04-4ddb-ac9d-29c05b9d4990.s3.amazonaws.com/example"
+ *                pageNum: 3
+ *    responses:
+ *      400:
+ *        $ref: '#/components/responses/BadRequest'
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      200:
+ *        description: A signed request object with the url to upload the file to S3
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                signedUrl:
+ *                  type: string
+ *                url:
+ *                  type: string
+ *                example:
+ *                  id: 17
+ *                  submissionId: 2
+ *                  type: "drawing"
+ *                  url: "https://bucketeer-0fcbcf8b-2b04-4ddb-ac9d-29c05b9d4990.s3.amazonaws.com/example"
+ *                  pageNum: 3
+ *                  created_at: "2022-07-26T02:59:49.621Z"
+ *                  updated_at: "2022-07-26T02:59:49.621Z"
+ */
 
-//save submissionPage with url
 router.post('/page', auth0Verify, authProfile, async (req, res) => {
   crudOperationsManager.post(
     res,
