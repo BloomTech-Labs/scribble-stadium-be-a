29fb2446d29f9bb1e80f1eb945a3fc6cec9e26c4
ashtilawat23 <ashtilawat23@gmail.com>
2022-08-22 23:26:21 -0400

    fixed linting errors

--- data/migrations/20220823025356_squads.js
+++ data/migrations/20220823025356_squads.js
@@ -1,18 +1,17 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('squads', (table) => {
-      table.increments('id').primary();
-      table.integer('childId');
-      table.integer('subId');
-      table.integer('teammateSubId');
-      table.integer('myDrawPoints');
-      table.integer('myWritePoints');
-      table.integer('teammateDrawPoints');
-      table.integer('teammateWritePoints');
-      table.timestamps(true, true);
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('squads');
-  };
-  
+  return knex.schema.createTable('squads', (table) => {
+    table.increments('id').primary();
+    table.integer('childId');
+    table.integer('subId');
+    table.integer('teammateSubId');
+    table.integer('myDrawPoints');
+    table.integer('myWritePoints');
+    table.integer('teammateDrawPoints');
+    table.integer('teammateWritePoints');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('squads');
+};
