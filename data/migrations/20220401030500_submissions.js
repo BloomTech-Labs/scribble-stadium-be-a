22f2ffc3a08e6a27a951fc4c851ab34943616882
ashtilawat23 <ashtilawat23@gmail.com>
2022-09-07 18:25:46 -0400

    updated sub seeds and squad script

--- data/migrations/20220401030500_submissions.js
+++ data/migrations/20220401030500_submissions.js
@@ -2,6 +2,8 @@ exports.up = function (knex) {
   return knex.schema.createTable('submissions', (table) => {
     table.increments('id').primary();
     table.integer('childId');
+    table.integer('squadId');
+    table.integer('matchupId');
     table.integer('storyId');
     table.integer('episodeId');
     table.timestamp('approvedAt');
