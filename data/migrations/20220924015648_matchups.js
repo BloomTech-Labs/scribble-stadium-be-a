2e4017e82b6878ae7abf1f75d24d979e9f4a3f42
ashtilawat23 <ashtilawat23@gmail.com>
2022-09-27 12:46:02 -0400

    lint fix

--- data/migrations/20220924015648_matchups.js
+++ data/migrations/20220924015648_matchups.js
@@ -1,12 +1,11 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('matchups', (table) => {
-      table.increments('id').primary();
-      table.timestamp('episodeDate');
-      table.timestamps(true, true);
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('matchups');
-  };
-  
+  return knex.schema.createTable('matchups', (table) => {
+    table.increments('id').primary();
+    table.timestamp('episodeDate');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('matchups');
+};
--- data/migrations/20220924015751_matchupQuadPlayers.js
+++ data/migrations/20220924015751_matchupQuadPlayers.js
@@ -1,14 +1,12 @@
-
 exports.up = function (knex) {
-    return knex.schema.createTable('matchupQuadPlayers', (table) => {
-      table.increments('id').primary();
-      table.integer('matchupId');
-      table.integer('matchupQuadId');
-      table.integer('subId')
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('matchupQuadPlayers');
-  };
-  
+  return knex.schema.createTable('matchupQuadPlayers', (table) => {
+    table.increments('id').primary();
+    table.integer('matchupId');
+    table.integer('matchupQuadId');
+    table.integer('subId');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('matchupQuadPlayers');
+};
